0fba7589fb6ab8fe4fc6f4c9556fcaa8508f2058
dmbch <daniel@dmbch.net>
2019-02-26 23:18:30 +0100

    refactor: significantly improve performance

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
-var execFile = require('child_process').execFile;
-var sep = require('path').sep;
-var eol = require('os').EOL;
+var fs = require('fs');
+var path = require('path');
 
 var cwd = process.env.INIT_CWD || process.cwd();
+var event = process.env.npm_lifecycle_event;
 
-function normalize(path) {
-  return path
-    .replace(new RegExp(sep, 'g'), '/')
+function normalize(module) {
+  return module
+    .replace(new RegExp(path.sep, 'g'), '/')
     .replace(new RegExp('^.*?/((?:@[^/]+/)?[^/]*)$'), '$1');
 }
 
@@ -25,26 +25,52 @@ function match(strings, patterns) {
 }
 
 function getAllDependencies() {
-  return new Promise(function(resolve, reject) {
-    function handleResult(error, stdout, stderr) {
-      if (stdout) {
-        var lines = stdout.split(eol).slice(1);
-        resolve(lines.map(normalize));
-      } else if (stderr) {
-        reject(new Error(stderr));
-      } else {
-        reject(error);
-      }
-    }
-    execFile('npm', ['ls', '--parseable'], { cwd: cwd }, handleResult);
-  });
+  function walk(dir) {
+    return new Promise(function(resolve) {
+      fs.readdir(dir, function(error, contents) {
+        error ? resolve([]) : resolve(contents);
+      });
+    }).then(function(contents) {
+      return Promise.all(
+        contents
+          .filter(function(curr) {
+            return !/^\./.test(curr);
+          })
+          .reduce(function(acc, curr) {
+            var module = path.join(dir, curr);
+            if (/^@/.test(curr)) {
+              acc.push(walk(module));
+            } else {
+              acc.push(Promise.resolve(module));
+              acc.push(
+                new Promise(function(resolve, reject) {
+                  fs.stat(module, function(error, stats) {
+                    error ? reject(error) : resolve(stats);
+                  });
+                }).then(function(stats) {
+                  if (stats.isDirectory()) {
+                    return walk(path.join(module, 'node_modules'));
+                  }
+                  return [];
+                })
+              );
+            }
+            return acc;
+          }, [])
+      ).then(function(results) {
+        return [].concat.apply([], results);
+      });
+    });
+  }
+  return walk(path.join(cwd, 'node_modules'));
 }
 
 function getAllDuplicates() {
   return getAllDependencies().then(function(dependencies) {
     return dependencies
-      .filter(function(curr, index) {
-        return dependencies.indexOf(curr) !== index;
+      .map(normalize)
+      .filter(function(curr, index, self) {
+        return self.indexOf(curr) !== index;
       })
       .filter(function(curr, index, self) {
         return self.indexOf(curr) === index;
@@ -54,6 +80,9 @@ function getAllDuplicates() {
 
 function getDuplicates() {
   var patterns = Array.prototype.slice.call(arguments);
+  if (!patterns.length && /install/.test(event)) {
+    patterns.push(normalize(process.cwd()));
+  }
   return getAllDuplicates().then(function(all) {
     var duplicates = patterns.length ? match(all, patterns) : all;
     return duplicates.sort();
