a4b346b37426caf1164b76021cda9e441ed82963
dmbch <daniel@dmbch.net>
2019-03-02 00:02:40 +0100

    refactor: make API synchronous
    BREAKING CHANGE: getDuplicates now returns a result instead of a promise

--- index.js
+++ index.js
@@ -4,101 +4,82 @@
 var fs = require('fs');
 var path = require('path');
 
-function getAllDependencies(cwd) {
-  function walk(dir) {
-    return new Promise(function(resolve) {
-      fs.readdir(dir, function(error, contents) {
-        error ? resolve([]) : resolve(contents);
-      });
-    }).then(function(contents) {
-      return Promise.all(
-        contents
-          .filter(function(curr) {
-            return !/^\./.test(curr);
-          })
-          .reduce(function(acc, curr) {
-            var module = path.join(dir, curr);
-            if (/^@/.test(curr)) {
-              acc.push(walk(module));
-            } else {
-              acc.push(Promise.resolve(module));
-              acc.push(
-                new Promise(function(resolve, reject) {
-                  fs.stat(module, function(error, stats) {
-                    error ? reject(error) : resolve(stats);
-                  });
-                }).then(function(stats) {
-                  if (stats.isDirectory()) {
-                    return walk(path.join(module, 'node_modules'));
-                  }
-                  return [];
-                })
-              );
-            }
-            return acc;
-          }, [])
-      ).then(function(results) {
-        return [].concat.apply([], results);
-      });
-    });
+function getDependencies(dir) {
+  if (fs.existsSync(dir)) {
+    var dependencies = fs
+      .readdirSync(dir)
+      .filter(function(curr) {
+        return !/^\./.test(curr);
+      })
+      .reduce(function(acc, curr) {
+        var currDir = path.join(dir, curr);
+        if (/^@/.test(curr)) {
+          acc.push(getDependencies(currDir));
+        } else {
+          acc.push(currDir);
+          if (fs.statSync(currDir).isDirectory()) {
+            var nextDir = path.join(currDir, 'node_modules');
+            acc.push(getDependencies(nextDir));
+          }
+        }
+        return acc;
+      }, []);
+    return [].concat.apply([], dependencies);
+  } else {
+    return [];
   }
-  return walk(path.join(cwd, 'node_modules'));
 }
 
 function getAllDuplicates(cwd) {
-  return getAllDependencies(cwd).then(function(dependencies) {
-    return dependencies
-      .map(function(curr) {
-        return curr
-          .replace(new RegExp(path.sep, 'g'), '/')
-          .replace(new RegExp('^.*?/((?:@[^/]+/)?[^/]*)$'), '$1');
-      })
-      .filter(function(curr, index, self) {
-        return self.indexOf(curr) !== index;
-      })
-      .filter(function(curr, index, self) {
-        return self.indexOf(curr) === index;
-      })
-      .sort();
-  });
+  return getDependencies(cwd)
+    .map(function(curr) {
+      return curr
+        .replace(new RegExp(path.sep, 'g'), '/')
+        .replace(new RegExp('^.*?/((?:@[^/]+/)?[^/]*)$'), '$1');
+    })
+    .filter(function(curr, index, self) {
+      return self.indexOf(curr) !== index;
+    })
+    .filter(function(curr, index, self) {
+      return self.indexOf(curr) === index;
+    })
+    .sort();
 }
 
 function getDuplicates(cwd) {
+  var dir = path.join(cwd, 'node_modules');
   var patterns = Array.prototype.slice.call(arguments, 1);
   var regExps = patterns.map(function(pattern) {
     return new RegExp('^' + pattern.replace(/\*+/g, '.*') + '$');
   });
-  return getAllDuplicates(cwd).then(function(duplicates) {
-    if (regExps.length) {
-      return duplicates.filter(function(curr) {
-        for (var i = 0; i < regExps.length; i++) {
-          if (regExps[i].test(curr)) return true;
-        }
-      });
-    }
-    return duplicates;
-  });
+  var duplicates = getAllDuplicates(dir);
+  if (regExps.length) {
+    return duplicates.filter(function(curr) {
+      for (var i = 0; i < regExps.length; i++) {
+        if (regExps[i].test(curr)) return true;
+      }
+    });
+  }
+  return duplicates;
 }
 
 if (require.main === module) {
   var cwd = process.env.INIT_CWD || process.cwd();
   var argv = process.argv.slice(2);
-  getDuplicates
-    .apply(null, [cwd].concat(argv))
-    .then(function(duplicates) {
-      duplicates.forEach(function(duplicate) {
-        // eslint-disable-next-line no-console
-        console.warn('Duplicate: %s', duplicate);
-      });
-      if (duplicates.length) {
-        process.exit(1);
-      }
-    })
-    .catch(function(error) {
+  try {
+    var duplicates = getDuplicates.apply(null, [cwd].concat(argv));
+    duplicates.forEach(function(duplicate) {
       // eslint-disable-next-line no-console
-      console.error(error);
-      process.exit(1);
+      console.warn('Duplicate: %s', duplicate);
     });
+    if (duplicates.length) {
+      process.exit(1);
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    process.exit(1);
+  }
 } else {
   module.exports = getDuplicates;
 }
