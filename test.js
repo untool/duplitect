22ec385060252e85a52c0c42d745860fe2cf9de1
dmbch <daniel@dmbch.net>
2019-07-15 10:48:12 +0200

    test: use different package in checks

--- test.js
+++ test.js
@@ -9,17 +9,17 @@ test('export test', function(t) {
 });
 
 test('basic API function test', function(t) {
-  var duplicates = getDuplicates(__dirname, 'debug');
+  var duplicates = getDuplicates(__dirname, 'supports-color');
   t.truthy(duplicates.length, 'duplicate detected');
-  t.is(duplicates[0], 'debug', 'expected duplicate found');
+  t.is(duplicates[0], 'supports-color', 'expected duplicate found');
 });
 
 test('basic CLI function test', function(t) {
   return new Promise(function(resolve, reject) {
-    execFile('./index.js', ['debug'], function(error, stdout, stderr) {
+    execFile('./index.js', ['supports-color'], function(error, stdout, stderr) {
       if (stderr) {
         t.truthy(stderr.length, 'duplicate detected');
-        t.regex(stderr, /debug/, 'expected duplicate found');
+        t.regex(stderr, /supports-color/, 'expected duplicate found');
         resolve();
       } else {
         reject(error);
