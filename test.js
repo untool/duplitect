db6b7cf3f65dc005f74f62d0224212677ad2d235
herschel666 <kraemerseele@gmx.de>
2020-05-18 13:55:38 +0200

    test: check new `getDuplicatesDetails` function

--- test.js
+++ test.js
@@ -1,16 +1,27 @@
 const test = require('ava');
 const { execFile } = require('child_process');
 
-const getDuplicates = require('./index');
+const { getDuplicates, getDuplicatesDetails } = require('./index');
 
 test('export test', (t) => {
   t.is(typeof getDuplicates, 'function', 'main export is a function');
 });
 
-test('basic API function test', (t) => {
+test('#getDuplicates: basic API function test', (t) => {
   const duplicates = getDuplicates(__dirname, 'supports-color');
   t.truthy(duplicates.length, 'duplicate detected');
   t.is(duplicates[0], 'supports-color', 'expected duplicate found');
+});
+
+test('#getDuplicatesDetails: basic API function test', async (t) => {
+  const duplicates = await getDuplicatesDetails(__dirname, 'supports-color');
+  const [{ name: nameOne }, { name: nameTwo }] = duplicates;
+  const [lowerVersion, higherVersion] = duplicates
+    .map(({ version }) => parseInt(version, 10))
+    .sort();
+  t.truthy(duplicates.length, 'duplicate detected');
+  t.is(nameOne, nameTwo, 'expected duplicate found');
+  t.true(lowerVersion < higherVersion, 'different major versions');
 });
 
 test('basic CLI function test', (t) =>
