901c4ed1401377a3591a867308f81f2ae5bae9a3
herschel666 <kraemerseele@gmx.de>
2020-05-18 13:49:08 +0200

    feat: add function for getting the versions of the duplicates
    ```js
    const { getDuplicatesDetails } = require('duplitect');
    
    (async () => {
      const duplicates = await getDuplicatesDetails(process.cwd(), 'some-package');
    
      duplicates.forEach(({ name, version, pathName }) => /* ... */);
    })();
    ```
    
    BREAKING CHANGE: `getDuplicates` is now a named export
    
    ```diff
    - const getDuplicates = require('duplitect');
    + const { getDuplicates } = require('duplitect');
    ```

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const {
   existsSync: exists,
   readdirSync: readdir,
   statSync: stat,
+  readFile: readFileCb,
 } = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+
+const readFile = promisify(readFileCb);
 
 function getDependencies(dir) {
   if (exists(dir)) {
@@ -31,8 +35,9 @@ function getDependencies(dir) {
   }
 }
 
-function getAllDuplicates(cwd) {
-  return getDependencies(cwd)
+function getAllDuplicates(cwd, getFullPaths = false) {
+  const dependencies = getDependencies(cwd);
+  const duplicates = dependencies
     .map((curr) =>
       curr
         .replace(/\\/g, '/')
@@ -41,6 +46,14 @@ function getAllDuplicates(cwd) {
     .filter((curr, index, self) => self.indexOf(curr) !== index)
     .filter((curr, index, self) => self.indexOf(curr) === index)
     .sort();
+
+  if (!getFullPaths) {
+    return duplicates;
+  }
+
+  return dependencies.filter((dep) =>
+    duplicates.some((dup) => dep.endsWith(dup))
+  );
 }
 
 function getDuplicates(cwd, ...patterns) {
@@ -57,6 +70,36 @@ function getDuplicates(cwd, ...patterns) {
     });
   }
   return duplicates;
+}
+
+async function getPackageVersion({ pathName, ...rest }) {
+  const pkg = await readFile(path.join(pathName, 'package.json'), 'utf8');
+  const { version } = JSON.parse(pkg);
+  return { pathName, version, ...rest };
+}
+
+async function getDuplicatesDetails(cwd, ...patterns) {
+  const dir = path.join(cwd, 'node_modules');
+  const regExps = patterns.map(
+    (pattern) => new RegExp(`node_modules\\/${pattern.replace(/\*+/g, '.*')}$`)
+  );
+  let duplicates = getAllDuplicates(dir, true);
+  if (regExps.length) {
+    duplicates = duplicates.filter((curr) => {
+      for (let i = 0; i < regExps.length; i++) {
+        if (regExps[i].test(curr)) return true;
+      }
+    });
+  }
+  const duplictesWithVersions = await Promise.all(
+    duplicates
+      .map((pathName) => ({
+        name: path.basename(pathName),
+        pathName,
+      }))
+      .map(getPackageVersion)
+  );
+  return duplictesWithVersions;
 }
 
 if (require.main === module) {
@@ -77,5 +120,5 @@ if (require.main === module) {
     process.exit(1);
   }
 } else {
-  module.exports = getDuplicates;
+  module.exports = { getDuplicates, getDuplicatesDetails };
 }
